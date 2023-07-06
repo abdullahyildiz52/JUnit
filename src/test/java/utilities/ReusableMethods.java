0dad8823ef5bd716125085dc3877d96ad7708271
abdullahyildiz52 <ce_abdullahyildiz@hotmail.com>
2023-07-06 16:39:24 +0300

    C03_WebTables

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,6 +1,8 @@
 package utilities;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.Set;
 
@@ -21,4 +23,10 @@ public class ReusableMethods {
         return ikinciSayfaWHD;
     }
 
+    public static String hucredekiYaziyiGetir(int satirNo, int sutunNo, WebDriver driver) {
+        //               //tbody/tr[7]/td[1]
+        String dinamikXPath = "//tbody/tr["+satirNo+"]/td["+sutunNo+"]";
+        WebElement istenenHucredekiElement = driver.findElement(By.xpath(dinamikXPath));
+        return istenenHucredekiElement.getText();
+    }
 }
