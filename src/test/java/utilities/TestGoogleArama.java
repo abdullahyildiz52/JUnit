a4818618031e93055d6ed99d7d486e9ee07cffe1
abdullahyildiz52 <ce_abdullahyildiz@hotmail.com>
2023-07-08 20:15:03 +0300

    C04_WebTables

---
+++ src/test/java/utilities/TestGoogleArama.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class TestGoogleArama {
+    @org.junit.Test
+    public void test01(){
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://google.com");
+        WebElement aramaKutusuElementi= driver.findElement(By.xpath("//textarea[@class=\"gLFyf\"]"));
+        aramaKutusuElementi.sendKeys("ARANILACAK METİN GİRİNİZ");
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.ENTER).perform();
+    }
+
+}
